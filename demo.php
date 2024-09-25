c9e30e94417662a580332b9ea1976fc53b7e7503
BuildTools <unconfigured@null.spigotmc.org>
2024-09-25 18:23:32 +0200

    Initiation auc  boucles

--- demo.php
+++ demo.php
@@ -1,22 +1,6 @@
 <?php
-$heure = (int)readline("Entrez une heure : ");
+$chiffre = null;
 
-if ((9 <= $heure && $heure <= 12) || (14 <= $heure && $heure <= 17)) {
-    echo "Le magasin sera ouvert";
-} else {
-    echo "Le magasin sera fermé";
+for ($i = 0; $i < 10; $i++) {
+    echo "- $i\n";
 }
-
-//switch ($action) {
-//    case 1:
-//        echo "J'attaque";
-//        break;
-//    case 2:
-//        echo "Je défends";
-//        break;
-//    case 3:
-//        echo "Je ne fais rien";
-//        break;
-//    default:
-//        echo "Commande inconnue";
-//}
