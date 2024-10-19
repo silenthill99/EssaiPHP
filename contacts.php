1f0b8d26f7291a63d93aa64a94ac0f2239054be0
silenthill99 <florian.graziani@sfr.fr>
2024-10-19 17:55:38 +0200

    Début d'apprentissage sur le PHP & HTML

---
+++ contacts.php
@@ -0,0 +1,7 @@
+<?php require "header.php" ?>
+
+<h2>Nous contacter</h2>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque, debitis dolorem error expedita facilis ipsum magnam
+    nostrum optio, pariatur quisquam sed totam. At dolores doloribus ex harum hic maxime ut!</p>
+
+<?php require "footer.php" ?>
---
+++ footer.php
@@ -0,0 +1,9 @@
+</main>
+<footer class="pt-5 my-5 text-body-secondary border-top">
+    Created by the Bootstrap team &middot; &copy; 2024
+</footer>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
